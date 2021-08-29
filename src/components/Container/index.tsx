caeef8ca078834d3d1e165ca3fadbb46e023b5ae
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-08-30 00:19:40 +0500

    !fix: container introduced

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const Container: FC = () => (
+  <h1>wow</h1>
+)
+
+export default Container;
---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  html,body, #root {
+    margin: 0;
+    padding: 0;
+    margin-left: 0;
+  }
+
+`
+
+export default {}
