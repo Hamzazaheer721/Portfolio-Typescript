86d03c94336e079ed988c3271674d523b355c04f
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-08-31 08:13:10 +0500

    !fix: header is adjusted

--- src/components/Header/index.styled.ts
+++ src/components/Header/index.styled.ts
@@ -2,30 +2,76 @@ import styled from 'styled-components'
 import {
   CloseOutlined,
   CustomerServiceOutlined,
-  FileOutlined, HomeOutlined, MessageOutlined, PictureOutlined, UserOutlined,
+  FileOutlined, HomeOutlined, MenuOutlined, MessageOutlined, PictureOutlined, UserOutlined,
 } from '@ant-design/icons'
 
-export const HeaderContainer = styled.div`
-  max-width: 768px;
-  margin-left: 15px;
-  margin-right: 15px;
+export const Label = styled.label`
+  color: white;
+  font-size: 2rem;
+  line-height: 80px;
+  padding: 0 100px;
+  font-weight: bold;
 `
-export const NavContainer = styled.div`
+export const MenuIcon = styled(MenuOutlined)`
+  font-size: 2rem;
+  color: white;
+  line-height: 80px;
+  float:right;
+  margin-right: 2rem !important;
+  cursor: pointer;  
+  display:none;
+`
+export const NavContainer = styled.nav`
+  background: #0082e6;
+  width: 100%;
+  height: 80px;
+  & .anticon{
+    margin: 0 5px;
+  }
+  & ul{
+    float: right;
+    margin-right: 20px;
+  }
+  & ul li {
+    display: inline-block;
+    margin: 10px 10px;
+  }
+  & ul li a{
+    color: white;
+    font-size: 1rem;
+    text-transform: uppercase;
+    border-radius: 13px;
+    
+  @media only screen and  (max-width: 952px){
+    ${Label}{
+      font-size: 1.5rem;
+      padding-left: 50px;
+    }
+    & ul li a{
+      font-size: 1rem;
+    }
+  }
 
+  @media only screen and (max-width : 858px){
+    ${MenuIcon}{
+      display:block !important;
+    }
+  } 
 `;
-
 export const UnOrderedList = styled.ul`
-  display: inline-block;
-  gap: 1.5px;
-  list-style-type: none;
-`
 
+`
 export const ListItem = styled.li`
+
 `
 
 export const AnchorTag = styled.a`
   text-decoration: none;
-  font-style: normal;
+  padding: 0.8rem;
+  &:active, :hover{
+    background: #1b9bff;
+    transition: 0.5s;
+  }  
 `
 export const HomeIcon = styled(HomeOutlined)`
 `
@@ -42,6 +88,7 @@ export const ContactIcon = styled(MessageOutlined)`
 
 // Nav Close Button
 export const NavCloseButton = styled(CloseOutlined)`
+  
 `
 
 export default {}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,63 +1,62 @@
 import { FC } from 'react';
 import {
   AboutIcon,
-  AnchorTag, ContactIcon, HeaderContainer, HomeIcon, ListItem,
-  NavCloseButton,
+  AnchorTag, ContactIcon, HomeIcon, Label, ListItem,
+  MenuIcon,
+  // NavCloseButton,
   NavContainer, PortfolioIcon, ServicesIcon, SkillsIcon, UnOrderedList,
 } from './index.styled';
 
 const Header: FC = () => (
-  <HeaderContainer>
-    <NavContainer>
-      {/* Portfolio Person */}
-      <AnchorTag>
-        Xapper7
-      </AnchorTag>
-      {/* Nav Items */}
-      <UnOrderedList>
-        <ListItem>
-          <AnchorTag href="#home">
-            <HomeIcon />
-            Home
-          </AnchorTag>
-        </ListItem>
-        <ListItem>
-          <AnchorTag href="#about">
-            <AboutIcon />
-            About
-          </AnchorTag>
-        </ListItem>
-        <ListItem>
-          <AnchorTag href="#skills">
-            <SkillsIcon />
-            Skills
-          </AnchorTag>
-        </ListItem>
-        <ListItem>
-          <AnchorTag href="#services">
-            <ServicesIcon />
-            Services
-          </AnchorTag>
-        </ListItem>
-        <ListItem>
-          <AnchorTag href="#portfolio">
-            <PortfolioIcon />
-            Portfolio
-          </AnchorTag>
-        </ListItem>
-        <ListItem>
-          <AnchorTag href="#contact">
-            <ContactIcon />
-            Contact
-          </AnchorTag>
-        </ListItem>
-      </UnOrderedList>
-      {/* Nav Close Button */}
-      <NavCloseButton />
-    </NavContainer>
+  <NavContainer>
+    <MenuIcon />
+    {/* Portfolio Person */}
+    <Label>
+      Hamza Zaheer
+    </Label>
+    {/* Nav Items */}
+    <UnOrderedList>
+      <ListItem>
+        <AnchorTag href="#home">
+          <HomeIcon />
+          Home
+        </AnchorTag>
+      </ListItem>
+      <ListItem>
+        <AnchorTag href="#about">
+          <AboutIcon />
+          About
+        </AnchorTag>
+      </ListItem>
+      <ListItem>
+        <AnchorTag href="#skills">
+          <SkillsIcon />
+          Skills
+        </AnchorTag>
+      </ListItem>
+      <ListItem>
+        <AnchorTag href="#services">
+          <ServicesIcon />
+          Services
+        </AnchorTag>
+      </ListItem>
+      <ListItem>
+        <AnchorTag href="#portfolio">
+          <PortfolioIcon />
+          Portfolio
+        </AnchorTag>
+      </ListItem>
+      <ListItem>
+        <AnchorTag href="#contact">
+          <ContactIcon />
+          Contact
+        </AnchorTag>
+      </ListItem>
+    </UnOrderedList>
+    {/* Nav Close Button */}
+    {/* <NavCloseButton /> */}
 
-  </HeaderContainer>
-
+  </NavContainer>
 )
 
 export default Header;
